62f1a344c4227b65cd2727160a2372adf1b03d05
clhcjy <1754598483@qq.com>
2024-11-19 16:53:09 +0800

    119陈烈烘

--- src/pycontent.py
+++ src/pycontent.py
@@ -1,10 +1,28 @@
 import datetime
 import openpyxl
+import pandas as pd
+from tqdm import tqdm
+from DrissionPage import ChromiumPage
+from urllib.parse import quote
+
+# 创建一个Chromium浏览器页面实例
+def create_page():
+    return ChromiumPage()
+
+contents = []
+
+# 创建页面实例
+page = create_page()
+
 
 # 打开工作簿
 workbook = openpyxl.load_workbook('eat.xlsx')
 # 获取表单
-sheet = workbook['表单1']
-# 读取指定的单元格数据
-cell = sheet.cell(row=1, column=1).value
-print(cell)
+sheet = workbook['Sheet1']
+
+for rows in sheet.iter_rows(min_row=2,min_col=2, max_col=2, max_row=67):
+        for row in rows:
+            print(row.value)
+
+
+
--- src/xiaohongshu.py
+++ src/xiaohongshu.py
@@ -3,6 +3,7 @@ import random
 import pandas as pd
 from tqdm import tqdm
 from DrissionPage import ChromiumPage
+from selenium.webdriver.common.by import By
 from urllib.parse import quote
 import openpyxl
 import logging
@@ -10,7 +11,6 @@ import logging
 # 配置日志记录器
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
-
 
 # 创建一个Chromium浏览器页面实例
 def create_page():
@@ -26,48 +26,39 @@ page = create_page()
 encoded_keyword = quote(keyword)
 # 构造搜索结果页面URL并请求
 page.get(f'https://www.xiaohongshu.com/search_result?keyword={encoded_keyword}&source=web_search_result_notes')
-
-
-page.scroll.to_bottom()    # 用于滚动页面到底部，水平位置不变
-
 def get_content():
     # 获取页面信息
-    # 获取包含所有搜索结果的容器元素
     container = page.ele('.feeds-page')
-    # log.info('获取搜索结果... container: %s', container)
-    # 获取所有搜索结果项
     sections = container.eles('.note-item')
-    # log.info('获取搜索结果... sections: %s', sections)
-    i = 1
     for section in sections:
-        i+=1
-        print(i)
+        print("section:",section)
         try:
-            # 获取每个搜索结果的链接
-            note_link = section.ele('tag:a', timeout=0).link
-            # log.info('获取搜索结果... note_link: %s', note_link)
-            # 获取每个搜索结果的标题
-            title = section.ele('.footer', timeout=0).ele('.title', timeout=0).text
-            # log.info('获取搜索结果... title: %s', title)
-            # 图片
-            img_url = section.ele('tag:img', timeout=0).link
-            log.info('获取搜索结果... img: %s', img_url)
-            # 将标题和链接添加到内容列表中
-            contents.append([title , note_link,img_url])
-        except Exception:  # 捕获所有异常，或者你可以指定具体的异常类型
-            continue  # 如果发生异常，跳过当前迭代
+            if section.ele('tag:play-icon'):
+                log.info("有视频,跳过")
+                continue
+            section.click()
+            content = page.ele('.note-detail-mask').ele('.note-container')
+            title = content.ele('.interaction-container').ele('.note-scroller').ele('.note-content').ele('.title', timeout=0).text
+            log.info(f"标题：{title}")
+            desc = content.ele('.interaction-container').ele('.note-scroller').ele('.note-content').ele('.desc', timeout=0).text
+            media_container = content.ele('.media-container')
+            img_url = media_container.ele('.slider-container').ele('.img-container').ele('tag:img', timeout=0).link
+            close_link = page.ele('.note-detail-mask').ele('.close-circle')
+            close_link.click()
+            contents.append([title, desc, img_url])
+        except Exception as e:
+            log.error(f"An error occurred: {e}")
+            continue
 
-    print("最终结果：$s",contents)
+# 滚动页面以加载更多内容
 
-for i in range(1,5):
-    get_content()
-    page.scroll.to_bottom()
-    
-name = ['title','note_link','img_url']
+get_content()
+page.scroll.to_bottom()
+time.sleep(1)  # 等待页面加载
+
+print("最终结果：", contents)
+
+name = ['标题', '内容', '图片链接']
 # 创建DataFrame对象并保存到Excel
 df = pd.DataFrame(data=contents, columns=name)
 df.to_excel('eat.xlsx', index=False)
-
-# page.ele('//*[@id="note-container"]')
-
-#  note-container
